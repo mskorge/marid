cd8aef233718c01e1ee4e022b6097b442f58f00c
Michael Skorge <michael@skorge.se>
2019-05-20 18:08:59 +0200

    Map additional variables

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,13 +8,16 @@ DJAVAX_NET_DEBUG=${DJAVAX_NET_DEBUG:-''}
 CONTAINER_IP=$(hostname -i)
 
 # Set config variables
-sed -i "s/^apiKey=.*$/apiKey=${MARID_API_KEY}/g" ${CONF_PATH}
-sed -i "s#\(^opsgenie\.api\.url=\)\(.*$\)#\1${MARID_API_URL}#g" ${CONF_PATH}
-sed -i "s/^maridKey=.*$/maridKey=${MARID_KEY}/g" ${CONF_PATH}
+sed -i "s/^apiKey\s=.*$/apiKey=${MARID_API_KEY}/g" $CONF_PATH
+sed -i "s#\(^opsgenie\.api\.url\s=\)\(.*$\)#\1${MARID_API_URL}#g" $CONF_PATH
+#sed -i "s/^\maridKey=.*$/maridKey=${MARID_KEY}/g" $CONF_PATH
 
-sed -i "s#\(^http\.server\.enabled=\)\(.*$\)#\1${MARID_HTTP_ENABLED}#g" ${CONF_PATH}
-sed -i "s#\(^http\.server\.host=\)\(.*$\)#\1${CONTAINER_IP}#g" ${CONF_PATH}
-sed -i "s#\(^http\.server\.port=\)\(.*$\)#\1${MARID_HTTP_PORT}#g" ${CONF_PATH}
+#sed -i "s#\(^http\.server\.enabled=\)\(.*$\)#\1${MARID_HTTP_ENABLED}#g" $CONF_PATH
+#sed -i "s#\(^http\.server\.host=\)\(.*$\)#\1${CONTAINER_IP}#g" $CONF_PATH
+#sed -i "s#\(^http\.server\.port=\)\(.*$\)#\1${MARID_HTTP_PORT}#g" $CONF_PATH
+
+sed -i "s#\(^librenms\.url\s=\)\(.*$\)#\1${MARID_LIBRENMS_URL}#g" $CONF_PATH
+sed -i "s#\(^librenms\.apiToken\s=\)\(.*$\)#\1${MARID_LIBRENMS_APITOKEN}#g" $CONF_PATH
 
 java \
   -Dmarid.config=/etc/opsgenie/marid \
