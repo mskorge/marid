b7e3062b0e11c07637f1feaa1dd423560c20dfa2
Michael Skorge <michael@skorge.se>
2019-05-20 18:18:46 +0200

    Damn you whitespace

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,8 +16,8 @@ sed -i "s#\(^opsgenie\.api\.url\s=\)\(.*$\)#\1${MARID_API_URL}#g" $CONF_PATH
 #sed -i "s#\(^http\.server\.host=\)\(.*$\)#\1${CONTAINER_IP}#g" $CONF_PATH
 #sed -i "s#\(^http\.server\.port=\)\(.*$\)#\1${MARID_HTTP_PORT}#g" $CONF_PATH
 
-sed -i "s#\(^librenms\.url\s=\)\(.*$\)#\1${MARID_LIBRENMS_URL}#g" $CONF_PATH
-sed -i "s#\(^librenms\.apiToken\s=\)\(.*$\)#\1${MARID_LIBRENMS_APITOKEN}#g" $CONF_PATH
+sed -i "s#\(^librenms\.url=\)\(.*$\)#\1${MARID_LIBRENMS_URL}#g" $CONF_PATH
+sed -i "s#\(^librenms\.apiToken=\)\(.*$\)#\1${MARID_LIBRENMS_APITOKEN}#g" $CONF_PATH
 
 java \
   -Dmarid.config=/etc/opsgenie/marid \
